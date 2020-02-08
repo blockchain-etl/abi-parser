05cd3f7502cb55fc8287f7664b5b56a37fd72b3f
medvedev1088 <medvedev1088@gmail.com>
2020-02-08 23:19:32 +0700

    Remove unneeded quote

--- frontend/src/components/Downloader.js
+++ frontend/src/components/Downloader.js
@@ -29,7 +29,7 @@ export class Downloader extends Component {
               }
             );
           }}
-        >Download Table Definitions for Events"</Button>
+        >Download Table Definitions for Events</Button>
       </div>
     )
   }
