9d230dde74b5d44092808d063e22aa5c51535ad2
Akshay <araa@connect.ust.hk>
2021-08-10 21:46:38 +0530

    update dependencies

--- frontend/src/components/TableLink.js
+++ frontend/src/components/TableLink.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class TableLink extends Component {
   render() {
     const { title, table } = this.props;
-    const tableData = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(table, null, 4));
+    const tableData =
+      "text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(table, null, 4));
     return (
-      <a href={`data: ${tableData}`} download={`${title}.json`}>{title}</a>
-    )
+      <a href={`data: ${tableData}`} download={`${title}.json`}>
+        {title}
+      </a>
+    );
   }
 }
 
-export default TableLink
+export default TableLink;
