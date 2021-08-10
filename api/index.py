415c6aa7919460c626b29676270354823a41348c
Akshay <araa@connect.ust.hk>
2021-08-10 13:57:30 +0530

    WIP: move secrets to .env + add method to read abi from polygonscan

--- api/index.py
+++ api/index.py
@@ -1,16 +1,23 @@
 import json
-
+import os
 from requests import get
 from jinja2 import Template
 from eth_utils import event_abi_to_log_topic, function_abi_to_4byte_selector
 from json import loads, dumps
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 PORT = 3000
-ETHERSCAN_API_KEY = '6H8VRTDHJVS6II983YY3DN8NCVBBHDA3MX' # should be env var, but whatever
+# should be env var, but whatever
+
+ETHERSCAN_API_KEY = os.environ.get("ETHERSCAN_API_KEY")
+POLYGONSCAN_API_KEY = os.environ.get("POLYGONSCAN_API_KEY")
 SOLIDITY_TO_BQ_TYPES = {
-  'address': 'STRING',
+    'address': 'STRING',
 }
 SQL_TEMPLATE_FOR_EVENT = '''
 CREATE TEMP FUNCTION
@@ -108,135 +115,165 @@ dataset_name = '<INSERT_DATASET_NAME>'
 table_prefix = '<TABLE_PREFIX>'
 table_description = ''
 
-### UTILS
+# UTILS
+
+
 def read_abi_from_address(address):
-  a = address.lower()
-  k = ETHERSCAN_API_KEY
-  url = f'https://api.etherscan.io/api?module=contract&action=getabi&address={a}&apikey={k}'
-  json_response = get(url).json()
-  return loads(json_response['result'])
+    a = address.lower()
+    k = ETHERSCAN_API_KEY
+    url = f'https://api.etherscan.io/api?module=contract&action=getabi&address={a}&apikey={k}'
+    json_response = get(url).json()
+    return loads(json_response['result'])
+
 
 def read_contract(contract):
-  if contract is not None and contract.startswith('0x'):
-    a = contract.lower()
-    k = ETHERSCAN_API_KEY
-    url = f'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={a}&apikey={k}'
-    json_response = get(url).json()
-    contract = [x for x in json_response['result'] if 'ContractName' in x][0]
-    return contract
-  else:
-    return {
-      'ContractName': 'unknown'
-    }
+    if contract is not None and contract.startswith('0x'):
+        a = contract.lower()
+        k = ETHERSCAN_API_KEY
+        url = f'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={a}&apikey={k}'
+        json_response = get(url).json()
+        contract = [x for x in json_response['result']
+                    if 'ContractName' in x][0]
+        return contract
+    else:
+        return {
+            'ContractName': 'unknown'
+        }
+
+
+def read_contract_polygon(contract):
+    if contract is not None and contract.startswith('0x'):
+        a = contract.lower()
+        k = POLYGONSCAN_API_KEY
+        url = f'https://api.polygonscan.com/api?module=contract&action=getsourcecode&address={a}&apikey={k}'
+        json_response = get(url).json()
+        contract = [x for x in json_response['result']
+                    if 'ContractName' in x][0]
+        return contract
+    else:
+        return {
+            'ContractName': 'unknown'
+        }
+
 
 def create_table_name(abi):
-  return table_prefix + '_event_' + abi['name']
+    return table_prefix + '_event_' + abi['name']
+
 
 def abi_to_table_definition(abi, contract_address, parser_type):
-  table_name = create_table_name(abi)
-  result = {}
-  result['parser'] = {
-    'type': parser_type,
-    'contract_address': contract_address,
-    'abi': abi,
-    'field_mapping': {}
-  }
+    table_name = create_table_name(abi)
+    result = {}
+    result['parser'] = {
+        'type': parser_type,
+        'contract_address': contract_address,
+        'abi': abi,
+        'field_mapping': {}
+    }
 
-  def transform_params(params):
-    transformed_params = []
-    for param in params:
-      if param.get('type') == 'tuple' and param.get('components') is not None:
-        transformed_params.append({
-          'name': param.get('name'),
-          'description': '',
-          'type': 'RECORD',
-          'fields': transform_params(param.get('components'))
-        })
-      else:
-        transformed_params.append({
-          'name': param.get('name'),
-          'description': '',
-          'type': 'STRING'  # we sometimes get parsing errors, so safest to make all STRING
-        })
-    return transformed_params
+    def transform_params(params):
+        transformed_params = []
+        for param in params:
+            if param.get('type') == 'tuple' and param.get('components') is not None:
+                transformed_params.append({
+                    'name': param.get('name'),
+                    'description': '',
+                    'type': 'RECORD',
+                    'fields': transform_params(param.get('components'))
+                })
+            else:
+                transformed_params.append({
+                    'name': param.get('name'),
+                    'description': '',
+                    'type': 'STRING'  # we sometimes get parsing errors, so safest to make all STRING
+                })
+        return transformed_params
 
-  result['table'] = {
-    'dataset_name': dataset_name,
-    'table_name': table_name,
-    'table_description': table_description,
-    'schema': transform_params(abi['inputs'])
-  }
-  return result
+    result['table'] = {
+        'dataset_name': dataset_name,
+        'table_name': table_name,
+        'table_description': table_description,
+        'schema': transform_params(abi['inputs'])
+    }
+    return result
+
 
 def contract_to_table_definitions(contract):
-  if contract is not None and contract.startswith('0x'):
-    contract_address = contract.lower()
-    abi = read_abi_from_address(contract)
-  else:
-    contract_address = 'unknown'
-    abi = json.loads(contract)
+    if contract is not None and contract.startswith('0x'):
+        contract_address = contract.lower()
+        abi = read_abi_from_address(contract)
+    else:
+        contract_address = 'unknown'
+        abi = json.loads(contract)
 
-  result = {}
-  for a in filter_by_type(abi, 'event'):
-    result[a['name']] = abi_to_table_definition(a, contract_address, 'log')
-  for a in filter_by_type(abi, 'function'):
-    result[a['name']] = abi_to_table_definition(a, contract_address, 'trace')
-  return result
+    result = {}
+    for a in filter_by_type(abi, 'event'):
+        result[a['name']] = abi_to_table_definition(a, contract_address, 'log')
+    for a in filter_by_type(abi, 'function'):
+        result[a['name']] = abi_to_table_definition(
+            a, contract_address, 'trace')
+    return result
 
 
 def s2bq_type(type):
-  return SOLIDITY_TO_BQ_TYPES.get(type, 'STRING')
+    return SOLIDITY_TO_BQ_TYPES.get(type, 'STRING')
+
 
 def filter_by_type(abi, type):
-  for a in abi:
-    if a['type'] == type:
-      yield a
+    for a in abi:
+        if a['type'] == type:
+            yield a
+
 
 def get_columns_from_event_abi(event_abi):
-  return [a.get('name') for a in event_abi['inputs']]
+    return [a.get('name') for a in event_abi['inputs']]
+
 
 def create_struct_fields_from_event_abi(event_abi):
-  return ', '.join(['`' + a.get('name') + '` ' + s2bq_type(a.get('type')) for a in event_abi['inputs']])
+    return ', '.join(['`' + a.get('name') + '` ' + s2bq_type(a.get('type')) for a in event_abi['inputs']])
+
 
 def abi_to_sql(abi, template, contract_address):
-  if abi['type'] == 'event':
-    selector = '0x' + event_abi_to_log_topic(abi).hex()
-  else:
-    selector = '0x' + function_abi_to_4byte_selector(abi).hex()
+    if abi['type'] == 'event':
+        selector = '0x' + event_abi_to_log_topic(abi).hex()
+    else:
+        selector = '0x' + function_abi_to_4byte_selector(abi).hex()
 
-  struct_fields = create_struct_fields_from_event_abi(abi)
-  columns = get_columns_from_event_abi(abi)
-  return template.render(
-    abi=dumps(abi),
-    contract_address=contract_address.lower(),
-    selector=selector,
-    struct_fields=struct_fields,
-    columns=columns
-  )
+    struct_fields = create_struct_fields_from_event_abi(abi)
+    columns = get_columns_from_event_abi(abi)
+    return template.render(
+        abi=dumps(abi),
+        contract_address=contract_address.lower(),
+        selector=selector,
+        struct_fields=struct_fields,
+        columns=columns
+    )
+
 
 def contract_to_sqls(contract):
-  if contract is not None and contract.startswith('0x'):
-    contract_address = contract.lower()
-    abi = read_abi_from_address(contract_address)
-  else:
-    contract_address = 'unknown'
-    abi = json.loads(contract)
+    if contract is not None and contract.startswith('0x'):
+        contract_address = contract.lower()
+        abi = read_abi_from_address(contract_address)
+    else:
+        contract_address = 'unknown'
+        abi = json.loads(contract)
 
-  event_tpl = Template(SQL_TEMPLATE_FOR_EVENT)
-  function_tpl = Template(SQL_TEMPLATE_FOR_FUNCTION)
+    event_tpl = Template(SQL_TEMPLATE_FOR_EVENT)
+    function_tpl = Template(SQL_TEMPLATE_FOR_FUNCTION)
 
-  result = {}
-  for a in filter_by_type(abi, 'event'):
-    result[a['name']] = abi_to_sql(a, event_tpl, contract_address)
-  for a in filter_by_type(abi, 'function'):
-    result[a['name']] = abi_to_sql(a, function_tpl, contract_address)
-  return result
+    result = {}
+    for a in filter_by_type(abi, 'event'):
+        result[a['name']] = abi_to_sql(a, event_tpl, contract_address)
+    for a in filter_by_type(abi, 'function'):
+        result[a['name']] = abi_to_sql(a, function_tpl, contract_address)
+    return result
 
-### WEB SERVER
+# WEB SERVER
+
 
 @app.route('/api/')
 def index():
     return jsonify({'status': 'alive'})
+
 
 @app.route('/api/test')
 def test():
@@ -248,15 +285,24 @@ def queries(contract):
     queries = contract_to_sqls(contract)
     return jsonify(queries)
 
+
 @app.route('/api/tables/<contract>')
 def tables(contract):
     tables = contract_to_table_definitions(contract)
     return jsonify(tables)
 
+
 @app.route('/api/contract/<contract>')
 def contract(contract):
     c = read_contract(contract)
     return jsonify(c)
+
+
+@app.route('/api/polygon/contract/<contract>')
+def contract_polygon(contract):
+    c = read_contract_polygon(contract)
+    return jsonify(c)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=PORT)
