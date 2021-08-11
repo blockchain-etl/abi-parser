52a0388b7761817c3be5787d54d001faa6a3b4fd
Akshay <araa@connect.ust.hk>
2021-08-11 12:16:10 +0530

    Cleanup + upgrade deps

--- frontend/src/components/TableDefinitions.js
+++ frontend/src/components/TableDefinitions.js
@@ -1,38 +1,50 @@
-import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card'
-import TableLink from './TableLink';
-import InputDataset from './InputDataset';
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+// import TableLink from "./TableLink";
+import InputDataset from "./InputDataset";
 
 export class TableDefinitions extends Component {
   render() {
     const {
       tables,
       contract,
-      queries,
+      //   queries,
       dataset,
       name,
       handleChangeDataset,
-      handleChangeContractName
+      handleChangeContractName,
     } = this.props;
-    const events = Object.entries(tables).filter(q => q[1].parser.type == 'log');
-    const functions = Object.entries(tables).filter(q => q[1].parser.type == 'trace');
+    const events = Object.entries(tables).filter(
+      (q) => q[1].parser.type === "log"
+    );
+    const functions = Object.entries(tables).filter(
+      (q) => q[1].parser.type === "trace"
+    );
     return (
       <Card className="m-3" style={{ width: 480 }} body>
-          <p>{`${events.length} events found in contract ${contract.ContractName}:`}</p>
-          <ol>{events.map(obj => <li>{obj[0]}</li>)}</ol>
-          <p>{`${functions.length} functions found in contract ${contract.ContractName}:`}</p>
-          <ol>{functions.map(obj => <li>{obj[0]}</li>)}</ol>
-          <InputDataset
-            tables={tables}
-            contract={contract}
-            dataset={dataset}
-            name={name}
-            handleChangeContractName={handleChangeContractName}
-            handleChangeDataset={handleChangeDataset}
-          />
-        </Card>
-    )
+        <p>{`${events.length} events found in contract ${contract.ContractName}:`}</p>
+        <ol>
+          {events.map((obj) => (
+            <li>{obj[0]}</li>
+          ))}
+        </ol>
+        <p>{`${functions.length} functions found in contract ${contract.ContractName}:`}</p>
+        <ol>
+          {functions.map((obj) => (
+            <li>{obj[0]}</li>
+          ))}
+        </ol>
+        <InputDataset
+          tables={tables}
+          contract={contract}
+          dataset={dataset}
+          name={name}
+          handleChangeContractName={handleChangeContractName}
+          handleChangeDataset={handleChangeDataset}
+        />
+      </Card>
+    );
   }
 }
 
-export default TableDefinitions
+export default TableDefinitions;
